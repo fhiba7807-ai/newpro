73cd3729b02721bdaa898bfa22a269afb17b53bf
hiba <fhiba7807@gmail.com>
2026-06-11 15:56:06 +0530

    even

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # print("hello world")
 # x=None
 # print(type(x))
-for i in range(1,10):
-    print(i)
-for i in range(20,30):
+# for i in range(1,10):
+#     print(i)
+for i in range(2,101,2):
     print(i)
